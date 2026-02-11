b24f0ed8591c21900fa74bae6893a8f8341bebb1
Florian Megnck <meganckflorian@gmail.com>
2026-02-11 12:09:10 +0100

    bugfix GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Lato } from "next/font/google";
+import { Suspense } from "react";
 import "./globals.css";
 
 const playfair = Playfair_Display({
@@ -36,7 +37,9 @@ export default function RootLayout({
         <Navbar />
         {children}
         <CookieBanner />
-        <GoogleAnalytics GA_MEASUREMENT_ID='G-XXXXXXXXXX' />
+        <Suspense fallback={null}>
+          <GoogleAnalytics GA_MEASUREMENT_ID='G-XXXXXXXXXX' />
+        </Suspense>
         <Footer />
       </body>
     </html>
