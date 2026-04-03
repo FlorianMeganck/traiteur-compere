c494633ac83c023f1c25158c274ebe03345857c2
Florian Megnck <meganckflorian@gmail.com>
2026-04-03 16:39:28 +0200

    modif logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -85,7 +85,7 @@ export default function Navbar() {
 
                     {/* CENTER ZONE: Logo */}
                     <div className="flex justify-center items-center">
-                        <Link href="/" className="relative block w-32 h-16 md:w-40 md:h-20 transition-transform hover:scale-105 z-50">
+                        <Link href="/" className="relative block w-40 h-22 md:w-40 md:h-20 transition-transform hover:scale-105 z-50">
                             <Image
                                 src="/images/Logo_traiteur.png"
                                 alt="Traiteur Compère"
