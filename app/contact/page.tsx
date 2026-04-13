c6f34d6422f126aff8abb29b7da553299389523f
Florian Megnck <meganckflorian@gmail.com>
2026-04-13 17:21:31 +0200

    cc traiteur

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -952,6 +952,7 @@ function ContactForm() {
             access_key: "32511cd2-dc66-49b5-8c6f-12a73315f644",
             subject: `Nouvelle demande : ${formData.Nom} ${formData.Prenom}`,
             from_name: "Site Traiteur Compère",
+            cc: "traiteurcompere@gmail.com",
 
             // DÉTAILS ÉVÉNEMENT
             "📋 Formule Choisie": formatFormulaName(menuParam),
