339ddb4ea98f90b3ee8a3e8e0789f8fc22271710
Florian Megnck <meganckflorian@gmail.com>
2026-04-23 23:40:01 +0200

    www sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://traiteur-compere.be'
+  const baseUrl = 'https://www.traiteur-compere.be'
 
   return [
     {
